b1047e5e603944e642b2c3ef362d405bb4012145
jonas lima <jlvieira248@gmail.com>
2023-02-13 21:37:02 -0300

    alterações trator

--- vamosplantar/controler/controlLoguin.js
+++ vamosplantar/controler/controlLoguin.js
@@ -34,7 +34,7 @@ function login() {
             auth = user;
             localStorage.setItem("auth",true);
             localStorage.setItem("user",document.getElementById("email").value);
-            window.location.href = "index.html";
+            window.location.href = "vamosplantar2023.html";
         }).catch(function (error) {
             alert("Falha ao logar");
 
